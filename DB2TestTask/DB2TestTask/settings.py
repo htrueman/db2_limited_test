9f6b8edb0338cb722c83554a5e17fc3c4ba62e36
Vadym Hevlich <vege1wgw@gmail.com>
2017-06-12 03:25:27 +0300

    heroku setup

--- DB2TestTask/DB2TestTask/settings.py
+++ DB2TestTask/DB2TestTask/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,10 +81,7 @@ WSGI_APPLICATION = 'DB2TestTask.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default="postgres:///db2_table", conn_max_age=500)
 }
 
 
@@ -130,6 +128,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
